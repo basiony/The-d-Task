c8c879d47debfbcceabbda106f804cb3342578c2
basiony <basyuunii@gmail.com>
2019-04-23 16:51:08 +0200

    My implementation for the task

--- app/src/main/java/com/example/thedtask/MainActivity.kt
+++ app/src/main/java/com/example/thedtask/MainActivity.kt
@@ -40,20 +40,18 @@ class MainActivity : AppCompatActivity() {
         //database instance
         var db = Room.databaseBuilder(applicationContext,AppDB::class.java,"itemDB").build()
 
-        //checking internet connectivity
-        val cm= baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        val networkInfo = cm.activeNetworkInfo
+        fun task() {
 
+            //checking internet connectivity
+            val cm= baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkInfo = cm.activeNetworkInfo
 
+            //if there is no internet connection, will retrieve item's data from database
+            if (networkInfo == null) {
+                Toast.makeText(this, "You are not connected to internet", Toast.LENGTH_LONG).show()
 
-
-        //if there is no internet connection, will retrieve item's data from database
-        if(networkInfo==null)
-        {
-            Toast.makeText(this,"You are not connected to internet",Toast.LENGTH_LONG).show()
-
-            /*Thread {
+                /*Thread {
                 var item = Item_Entity()
 
                 db.itemDAO().readItem().forEach{
@@ -65,111 +63,110 @@ class MainActivity : AppCompatActivity() {
             }.start()*/
 
 
-            //get data from cache if there is no network
+                //get data from cache if there is no network
 
-            AndroidNetworking.initialize(this)
-            AndroidNetworking.get("http://limitless-forest-98976.herokuapp.com/").getResponseOnlyIfCached().build()
-                .getAsObject(
-                    ItemDetails::class.java,
-                    object : ParsedRequestListener<ItemDetails> {
-                        override fun onResponse(response: ItemDetails) {
-                            itemList.addAll(response.data)
-                            myAdapter.notifyDataSetChanged()
+                AndroidNetworking.initialize(this)
+                AndroidNetworking.get("http://limitless-forest-98976.herokuapp.com/").getResponseOnlyIfCached().build()
+                    .getAsObject(
+                        ItemDetails::class.java,
+                        object : ParsedRequestListener<ItemDetails> {
+                            override fun onResponse(response: ItemDetails) {
+                                itemList.addAll(response.data)
+                                myAdapter.notifyDataSetChanged()
+                            }
+
+
+                            override fun onError(anError: ANError?) {
+
+                            }
+
+
                         }
 
 
-                        override fun onError(anError: ANError?) {
+                    )
+
+
+            } else {
+
+                //get data from server if the device is connected to the internet
+
+                AndroidNetworking.initialize(this)
+                AndroidNetworking.get("http://limitless-forest-98976.herokuapp.com/").build()
+                    .getAsObject(
+                        ItemDetails::class.java,
+                        object : ParsedRequestListener<ItemDetails> {
+                            override fun onResponse(response: ItemDetails) {
+                                itemList.addAll(response.data)
+                                myAdapter.notifyDataSetChanged()
+
+
+                                //saving in database using room library. I tried to save images in it but i found that it is not recommended, so i used the AndroidNetworking library method for caching (getResponseOnlyIfCached())
+
+
+                                Thread {
+                                    var item = Item_Entity()
+
+
+                                    //saving each item in the Item_Entity table
+
+                                    itemList.forEach {
+
+                                        item.item_title = it.name
+                                        item.item_price = it.price
+                                        item.item_description = it.productDescription
+
+
+
+                                        db.itemDAO().saveItem(item)
+
+                                    }
+
+
+                                    //testing that items are saved successfully
+
+                                    db.itemDAO().readItem().forEach {
+                                        Log.i("ayhaga", "id:${it.item_id}")
+                                        Log.i("ayhaga", "title: ${it.item_title}")
+                                        Log.i("ayhaga", "title: ${it.item_price}")
+                                        Log.i("ayhaga", "desc: ${it.item_description}")
+
+                                    }
+
+
+                                }.start()
+
+
+                            }
+
+
+                            override fun onError(anError: ANError?) {
+
+                            }
+
 
                         }
 
 
-                    }
+                    )
 
-
-                )
-
-
-
+            }
         }
 
 
-      else {
-
-            //get data from server if the device is connected to the internet
-
-            AndroidNetworking.initialize(this)
-            AndroidNetworking.get("http://limitless-forest-98976.herokuapp.com/").build()
-                .getAsObject(
-                    ItemDetails::class.java,
-                    object : ParsedRequestListener<ItemDetails> {
-                        override fun onResponse(response: ItemDetails) {
-                            itemList.addAll(response.data)
-                            myAdapter.notifyDataSetChanged()
-
-
-                            //saving in database using room library. I tried to save images in it but i found that it is not recommended, so i used the AndroidNetworking library method for caching (getResponseOnlyIfCached())
-
-
-                            Thread {
-                                var item = Item_Entity()
-
-
-                                //saving each item in the Item_Entity table
-
-                                itemList.forEach {
-
-                                    item.item_title = it.name
-                                    item.item_price = it.price
-                                    item.item_description = it.productDescription
-
-
-
-                                    db.itemDAO().saveItem(item)
-
-                                }
-
-
-                                //testing that items are saved successfully
-
-                                db.itemDAO().readItem().forEach {
-                                    Log.i("ayhaga", "id:${it.item_id}")
-                                    Log.i("ayhaga", "title: ${it.item_title}")
-                                    Log.i("ayhaga", "title: ${it.item_price}")
-                                    Log.i("ayhaga", "desc: ${it.item_description}")
-
-                                }
-
-
-                            }.start()
-
-
-                        }
-
-
-                        override fun onError(anError: ANError?) {
-
-                        }
-
-
-                    }
-
-
-                )
-
-        }
 
 
 
 
+        task()
 
 
-
-
-        //SwipeRefreshLayout
+        // handling the SwipeRefreshLayout
         val refresh=findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
         refresh.setOnRefreshListener {
             Handler(Looper.getMainLooper()).postDelayed(
                 {
+                    task()
                     myAdapter.notifyDataSetChanged()
                     refresh.isRefreshing=false
 
